4b0fa67c917438df4c081ed8a251d8ada2539c09
Allenade <allenumunade@gmail.com>
2023-11-29 21:33:06 +0100

    solve

--- src/component/progress.jsx
+++ src/component/progress.jsx
@@ -1,33 +1,30 @@
-// App.js
-
 import { useState, useEffect } from "react";
 
 const Progress = () => {
   const [raisedAmount, setRaisedAmount] = useState(0);
-  const targetAmount = 100000000; // $100 million
+  const targetAmount = 100000; // $100,000
 
   useEffect(() => {
     const interval = setInterval(() => {
       setRaisedAmount((prevAmount) =>
-        prevAmount < targetAmount ? prevAmount + 100000 : prevAmount
+        prevAmount < targetAmount ? prevAmount + 1000 : prevAmount
       );
-    }, 600000); // 600000 milliseconds = 10 minutes
+    }, 6000); // 60000 milliseconds = 1 minute
 
     return () => clearInterval(interval);
   }, []);
 
-  //   const progress = (raisedAmount / targetAmount) * 100;
+  const progress = (raisedAmount / targetAmount) * 100;
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="p-2 mb-4 text-2xl font-bold md:p-4">
-        Fundraising Progress
-      </h1>
-      {/* <div className="w-1/2 h-4 mb-4 bg-blue-500">
+      <h1 className="mb-4 text-2xl font-bold">Fundraising Progress</h1>
+      <div className="w-1/2 h-4 mb-4 bg-blue-500">
         <div
-          className={`bg-green-500 h-full transition-width duration-500 ease-in-out w-${progress}`}
+          className="h-full duration-500 ease-in-out bg-green-500 transition-width"
+          style={{ width: `${progress}%` }}
         ></div>
-      </div> */}
+      </div>
       <p>
         Raised: ${raisedAmount.toLocaleString()} | Target: $
         {targetAmount.toLocaleString()}
